e82bc42b61b4e1a6532e2b6d1cdb32dbfc747a64
yasirbil <yasirbil@gmail.com>
2020-05-23 11:31:00 +0000

    Terraform first Class

---
+++ key_pair.tf
@@ -0,0 +1,4 @@
+resource "aws_key_pair" "us-east-1-key" {
+  key_name   = "yasirs_bastion"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQD4goLLf+t4U3Qt3QbKvztrrSlavNPMjFYGIrxVomQVyNnapMgeP6qD/BPZpkbwBunkzDU2W70cFdFmIWr2biOas+8dFfGq0jh66zfPxj20+z9agR19aDWPjzzjUr5MLek/lVqn4Ux3yvWN8p9vaXB+emtcZAYHNH/WRkl6sTPw4IBxGQ8AZQmqfbJWu6hQ6fVebKtaTUYxee7mVRtgv2ZzveYm4GrFwXRmQAIaodt8+SeqTTY8l6hdZqoPDSKt2fO6jhu54vNFQvQ9q6/Smv9g0FvXXKaGzcN+vHNC6m4Ax+I/6iOikNXf6YtomM6SfMyuOP7tMcZBP9lAwKMDYUyB root@ip-172-31-23-72.eu-west-2.compute.internal"
+}
---
+++ provider.tf
@@ -0,0 +1,8 @@
+provider "aws" {
+  region = "us-east-1"
+  version = "2.59"
+}
+
+terraform {
+  required_version = "0.11.14"
+}
